34be55ebc89930103ee3cdcbfe3104609ea94959
Matúš Jókay <matus.jokay@gmail.com>
2020-12-07 13:14:04 +0100

    Add files via upload

---
+++ 2020/03/03.py
@@ -0,0 +1,31 @@
+
+def slope(row, col):
+    act_col = 0
+    res = 0
+    wait_row = row
+    for line in open('input.txt'):     
+        if wait_row > 0:
+            wait_row -= 1
+            continue
+
+        wait_row = row - 1
+        line = line.strip()
+        act_col = (act_col + col) % len(line)    
+        c = line[act_col]
+        if c == '#':
+            res += 1
+        #print(c)
+            
+    return res
+
+# part 1
+print(slope(1,3))
+
+# part 2
+mul = 1
+for s in [(1,1), (1,3),(1,5), (1,7), (2,1)]:
+    m = slope(*s)
+    mul *= m
+print(mul)
+
+    
