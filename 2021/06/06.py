31f37fa8e93ddf2393c36e2d826b52a0d7484c87
Matus Jokay <matus.jokay@gmail.com>
2021-12-06 12:35:45 +0100

    refactor(2021): day 6: solution without recursion

--- 2021/06/06.py
+++ 2021/06/06.py
@@ -17,3 +17,24 @@ nums = [*map(int, open('input.txt').readline().split(','))]
 
 print(do_day6(nums, cnt = 80))
 print(do_day6(nums, cnt = 256))
+
+
+#########################################
+#########################################
+#########################################
+
+
+nums = [*map(int, open('input.txt').readline().split(','))]
+
+def do(nums, days):
+    state = [nums.count(i) if i in nums else 0 for i in range(9)]
+    for i in range(days):
+        # each day:
+        #    1 becomes 0, 2 becomes 1, 3 becomes 2, ..., 8 becomes 7
+        #    0 becomes 6 and 7 becomes 6, too
+        #    0 adds a new 8
+        state = state[1:7] + [state[0]+state[7]] + [state[8]] + [state[0]]
+    return sum(state)
+
+print(do(nums, days=80))
+print(do(nums, days=256))
