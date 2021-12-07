5f61793302ef7b1691686a09b1affa5b614c8f7a
Matus Jokay <matus.jokay@gmail.com>
2021-12-07 20:31:26 +0100

    style(2021): day7

--- 2021/07/07.py
+++ 2021/07/07.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 nums = [*map(int, open('input.txt').readline().split(','))]
-print(min([sum([abs(n-pos) for n in nums]) for pos in range(max(nums))]))
-print(min([sum([sum(range(abs(n-pos)+1)) for n in nums]) for pos in range(max(nums))]))
+print(min(sum(abs(n-pos) for n in nums) for pos in range(max(nums))))
+print(min(sum(sum(range(abs(n-pos)+1)) for n in nums) for pos in range(max(nums))))
