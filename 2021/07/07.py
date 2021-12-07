b08b40cd08b8a5f666b16bd73527cdfd6eafeb92
Matus Jokay <matus.jokay@gmail.com>
2021-12-07 10:01:03 +0100

    refactor(2021): day7: improved version 2

--- 2021/07/07.py
+++ 2021/07/07.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
 
-def do(nums, fuel_cost):
-    print([sum([fuel_cost(n, pos) for n in nums]) for pos in range(max(nums))])
-
-
 nums = [*map(int, open('input.txt').readline().split(','))]
-do(nums, lambda n,pos: abs(n-pos))
-do(nums, lambda n,pos: sum(range(abs(n-pos)+1)))
+print(min([sum([abs(n-pos) for n in nums]) for pos in range(max(nums))]))
+print(min([sum([sum(range(abs(n-pos)+1)) for n in nums]) for pos in range(max(nums))]))
