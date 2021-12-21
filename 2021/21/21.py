ff39c7dc3e9cc42440f98637828368d15b447ba8
Matus Jokay <matus.jokay@gmail.com>
2021-12-21 22:18:58 +0100

    refactor(2021): day 21: improved version

--- 2021/21/21.py
+++ 2021/21/21.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 
+from functools import cache
+
+
 def partA(board):
     player = 1
     score = [0, 0]
@@ -20,36 +23,32 @@ def partA(board):
             return
 
 
-def play(board, score=[0, 0], move=None, turn=1):
-    # reduced states after three rolls of Dirac dice
-    dd = {3:1, 4:3, 5:6, 6:7, 7:6, 8:3, 9:1}
-
+@cache
+def play(play_cur, play_next, score_cur=0, score_next=0, move=None):
     if move:
         # update state of the game board for a player in this turn
-        board[turn % 2] = (board[turn % 2] + move) % 10
-        if not board[turn % 2]:
-            board[turn % 2] = 10
-        score[turn % 2] += board[turn % 2]
+        play_cur = (play_cur + move) % 10
+        if not play_cur:
+            play_cur = 10
+        score_cur += play_cur
 
-        # check win condition: res[0] is set to 1 if player 1 wins,
-        # res[1] is set to 1 if player 2 wins
-        res = [0, 0]
-        if score[turn % 2] >= 21:
-            res[turn % 2] = 1
-            return res
+        # check win condition
+        if score_cur >= 21:
+            return 0, 1
 
     # if no player wins, go to the next turn of the game:
     # split the universe into **seven** copies
     # (reduced states after three rolls of Dirac dice)
+    dd = {3:1, 4:3, 5:6, 6:7, 7:6, 8:3, 9:1}
     win = [0, 0]
     for move in dd:
-        ret = play(board[:], score[:], move, turn+1)
-        win = win[0] + ret[0] * dd[move], win[1] + ret[1] * dd[move]
+        ret = play(play_next, play_cur, score_next, score_cur, move)
+        win = win[0] + ret[1] * dd[move], win[1] + ret[0] * dd[move]
     return win
 
 
 def partB(board):
-    res = play(board)
+    res = play(board[1], board[0])
     print(max(res))
 
 
