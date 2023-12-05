460e478feb03d2eee05fbf2b84f4a44269d0eb27
Matus Jokay <matus.jokay@gmail.com>
2023-12-05 13:53:47 +0100

    refactor(2023): day 3: optimalized solution

--- 2023/03/03.py
+++ 2023/03/03.py
@@ -2,50 +2,29 @@
 
 import re
 
-# read input map
-f = [line.strip() for line in open('input.txt')]
-
-
-# part A
-
-def connected(match: re.Match, line: int, m: list[list[str]]) -> bool:
-    # create initial neighbours coordinates for the number based on its span
-    nghbrs = [(match.span()[0]-1, line), (match.span()[1], line)]
-    nghbrs += [(i, line-1) for i in range(match.span()[0]-1, match.span()[1]+1)]
-    nghbrs += [(i, line+1) for i in range(match.span()[0]-1, match.span()[1]+1)]
-    # filter out coordinates out of the map
-    nghbrs = filter(lambda n: n[0] >= 0 and n[0] < len(m[0]) and n[1] >= 0 and n[1] < len(m), nghbrs)
-    # filter only those neighbours which are special symbols
-    nghbrs = list(filter(lambda n: m[n[1]][n[0]] != '.', nghbrs))
-    # if the number is not adjacent to a symbol, @nghbrs is an empty list
-    return True if nghbrs else False
-
-# find all numbers in the input
-m = [(match, i) for i, l in enumerate(f) for match in re.finditer(r'(\d+)', l)]
-# process all numbers: get only numbers adjacent to a symbol
-print(sum([int(match[0].group()) if connected(match[0], match[1], f) else 0 for match in m]))
-
-
-# part B
-
-def gear(x: int, y: int, m: list[list[str]], nums: dict[tuple[int,int],str]) -> int:
-    # create initial neighbours coordinates for the star
+# x, y: coordinates, m: input map, nums: coords -> num mapping, ver: 'A' or 'B'
+def do_neighbors(x, y, m, nums, ver):
+    # create and filter coordinates of neighbors
     nghbrs = [(x+1,y+1),(x+1,y),(x+1,y-1),(x,y-1),(x,y+1),(x-1,y+1),(x-1,y),(x-1,y-1)]
-    # filter out coordinates out of the map
     nghbrs = filter(lambda n: n[0] >= 0 and n[0] < len(m[0]) and n[1] >= 0 and n[1] < len(m), nghbrs)
-    # filter only those star's neighbours which are digits (i.e. part of some number)
     nghbrs = list(filter(lambda n: m[n[1]][n[0]].isdigit(), nghbrs))
 
     # create set of numbers corresponding to filtered coordinates
-    ret = list(set(map(lambda n: int(nums[n]), nghbrs)))
-    # return gear ratio
-    return 0 if len(ret) == 1 else ret[0] * ret[1]
+    # If star is not a gear, there is only one number in a list, so append zero to get
+    # two elements in a @ret and resulting zero in multiplication ret[0]*ret[1]
+    ret = list(set(map(lambda n: int(nums[n]), nghbrs))) + [0]
 
+    return ret[0] * ret[1] if ver == 'B' else sum(ret)
+
+
+f = [line.strip() for line in open('input.txt')]
+
+# find all numbers in the input
+m = [(match, i) for i, l in enumerate(f) for match in re.finditer(r'(\d+)', l)]
 # for each input number create a mapping between its coordinates and the number itself
 nums = {(k, line): match.group() for match, line in m for k in range(*match.span())}
-# find all specials
-stars = [(match, i) for i, l in enumerate(f) for match in re.finditer(r'(\*+)', l)]
-# find all stars in the input
-stars = [(match, i) for i, l in enumerate(f) for match in re.finditer(r'(\*+)', l)]
-# process stars
-print(sum([gear(match[0].span()[0], match[1], f, nums) for match in stars]))
+# find all specials in the input
+specs = [(match, i) for i, l in enumerate(f) for match in re.finditer(r'([^0-9.]+)', l)]
+
+print(sum([do_neighbors(match[0].span()[0], match[1], f, nums, 'A') for match in specs]))
+print(sum([do_neighbors(match[0].span()[0], match[1], f, nums, 'B') for match in specs]))
