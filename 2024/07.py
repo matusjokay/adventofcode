285b4ddb04ba3eeb8dc932abc5b4450326bcca3c
Matus Jokay <matus.jokay@gmail.com>
2024-12-09 00:04:36 +0100

    feat(2024): day 7: more pythonic changes forgotten in previous commit

--- 2024/07.py
+++ 2024/07.py
@@ -12,14 +12,8 @@ f = [[map(int,l.split()) for l in line.strip().split(': ')] for line in open('07
 def test(e, op=None):
     ops = [mul, add] if not op else [mul, add, op]
     r = [reduce(lambda x,y: (x[1](x[0], y[0]), y[1]), zip(e[1], p+(None,)), (0, add))[0] for p in product(ops, repeat=len(e[1])-1)]
-    return filter(lambda x: str(e[0][0]).startswith(str(x)) or str(e[0][0]).endswith(str(x)), r)
+    return filter(lambda x: e[0][0] == x, r)
 
 
-part_A = filter(lambda x: x[0][0] in test(x), f)
-s_A = sum([line[0][0] for line in part_A])
-print(s_A)
-
-
-part_B = filter(lambda x: x[0][0] in test(x, lambda x,y: int(str(x)+str(y))), f)
-s_B = sum([line[0][0] for line in part_B])
-print(s_B)
+print(sum([i[0][0] for i in filter(lambda x: x[0][0] in test(x), f)]))
+print(sum([i[0][0] for i in filter(lambda x: x[0][0] in test(x, lambda x,y: int(str(x)+str(y))), f)]))
